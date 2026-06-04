8d59d46ecffe8fc3e5c11df5df31ebee6daef282
SudoMakeMeASandwichDE <sudosandwich.contact@gmail.com>
2026-06-04 17:57:52 +0200

    multiselect and color change for selection

--- scripts/troupe.gd
+++ scripts/troupe.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 @export var waypoint: PackedScene
 
 @export var radius := 20.0 # circle texture radius
-@export var color := Color.RED 
+@export var color_unselected := Color(1, 0, 0, 1)
+@export var color_selected := Color(0.833, 0.0, 0.0, 1.0)
 var waypoints := [] # waypoind Vector2 coords
 var selected := false # apply new waypoints if true
 var target: Vector2 # next waypoint
@@ -12,8 +13,9 @@ var path_end := false # true when troupe reached last waypoint
 var waypoint_objs := [] # collects waypoint game objects
 var moving := false
 
-static var selected_troupe: CharacterBody2D = null
+static var selected_troupe: Array[CharacterBody2D] = []
 
+var color = color_unselected
 func _draw():
 	draw_circle(Vector2.ZERO, radius, color)
 
@@ -24,19 +26,28 @@ func _ready():
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("waypoint_on_cursor"):
 		# spawn waypoint on mouse click
-		if selected_troupe == self:
-				var obj = waypoint.instantiate()
-				waypoint_objs.append(obj)
-				obj.global_position = get_global_mouse_position()
-				print(obj.global_position)
-				get_tree().get_current_scene().add_child(obj)
-				waypoints.append(obj.global_position)
+		if self in selected_troupe:
+			var obj = waypoint.instantiate()
+			waypoint_objs.append(obj)
+			obj.global_position = get_global_mouse_position()
+			print(obj.global_position)
+			get_tree().get_current_scene().add_child(obj)
+			waypoints.append(obj.global_position)
 	elif event.is_action_pressed("select"):
+		var troupes = get_tree().get_nodes_in_group("troupes")
+		for troupe in troupes:
+			if troupe.moving:
+				selected_troupe.erase(troupe)
+				troupe.toggle_color()
+		if self in selected_troupe:
+			return
 		if global_position.distance_to(get_global_mouse_position()) <= 10: # select troupe with mouse click
 			reset()
-			selected_troupe = self
+			selected_troupe.append(self)
+			toggle_color()
+			get_viewport().set_input_as_handled()
 			return
-	elif event.is_action_pressed("cancel") and selected_troupe == self:
+	elif event.is_action_pressed("cancel") and self in selected_troupe:
 		reset()
 			
 func _process(delta: float) -> void:
@@ -56,11 +67,21 @@ func _process(delta: float) -> void:
 func reset():
 	# reset everything eg when last waypoint is reached
 	path_end = true
+	moving = false
 	waypoints.clear()
 	print("Path ended")
 	target_num = 0
-	selected_troupe = null
+	if self in selected_troupe:
+		selected_troupe.erase(self)
+		toggle_color()
 	for obj in waypoint_objs:
 		obj.queue_free()
 	waypoint_objs.clear()
+	
+func toggle_color():
+	if self in selected_troupe:
+		color = color_selected
+	else:
+		color = color_unselected
+	queue_redraw()
 	
